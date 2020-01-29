d56388e1a56ed8e0905dcb0f13e28451e3282e34
Jose Daniel Osorio Morales <josorio@personalsoft.com.co>
2020-01-29 15:12:35 -0500

    Tareas realizadas: Se crean componentes, se agregan rutas en app-routing, se descarga y configura bootstrap.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'shoppingcart', component: ShoppingcartComponent},
+  {path: 'category', component: CategoryComponent},
+  {path: 'contactus', component: ContactComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    ShoppingcartComponent,
+    ProductsComponent,
+    CategoryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,

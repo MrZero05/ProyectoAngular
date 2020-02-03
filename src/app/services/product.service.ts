f7b43d45632da0f3f12ec304c6dda344c3375c75
sergio arcon <searcon@personalsoft.com.co>
2020-02-03 16:22:30 -0500

    Se agrega indicador de cantidad al logo del carrito de compras en header

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,6 +13,6 @@ export class ProductService {
   getProductByCategory(category: string): Observable<Product[]> {
     // call service api for product list filter y category
     console.log(category);
-    return this.http.get<Product[]>('./assets/fakeServerData/products.json');
+    return this.http.get<Product[]>('http://172.19.12.163:3000/products');
   }
 }

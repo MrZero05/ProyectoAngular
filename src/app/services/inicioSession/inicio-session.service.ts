0d5b48455f1d7665a0fae36adb29a8797691d15c
sergio arcon <searcon@personalsoft.com.co>
2020-02-13 14:35:02 -0500

    fix save token into localStorage

--- src/app/services/inicioSession/inicio-session.service.ts
+++ src/app/services/inicioSession/inicio-session.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { InicioSessionDTO } from '../../dto/inicio-session-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +9,8 @@ export class InicioSessionService {
 
   constructor(private http: HttpClient) { }
 
-  iniciarSession(param: any): Observable<InicioSessionDTO>{
-     return this.http.post<any>('http://localhost:8080/api/login', param);
+  iniciarSession(param: any): Observable<any> {
+    return this.http.post<any>('http://localhost:8080/api/login', param);
   }
+
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,13 +13,13 @@ export class ProductService {
   getProductByCategory(category: string): Observable<Product[]> {
     // call service api for product list filter y category
     console.log(category);
-    return this.http.get<Product[]>('http://localhost:8080/producto/listProducto/' + category);
+    return this.http.get<Product[]>('http://localhost:8080/api/producto/listProducto/' + category);
   }
 
   getProductById(id: string): Observable<Product> {
     // Call service Api to bring a list of product fitlered by id
     console.log(id);
-    return this.http.get<Product>('http://localhost:8080/producto/getProducto/' + id);
+    return this.http.get<Product>('http://localhost:8080/api/producto/getProducto/' + id);
 
   }
 }

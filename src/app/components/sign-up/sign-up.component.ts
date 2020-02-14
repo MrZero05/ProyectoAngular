49032112da3a2ba6c7c5b5b2ab45ace9e43c9f77
Jose Daniel Osorio Morales <josorio@personalsoft.com.co>
2020-02-14 08:51:51 -0500

    servicio de registro

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SignUpService } from 'src/app/services/sign-up.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,15 +11,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SignUpComponent implements OnInit {
   signUpForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private service: SignUpService, private router: Router) {
 
     this.signUpForm = this.fb.group({
+      username: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]]
     });
    }
 
   ngOnInit() {
+  }
+
+  registerSession() {
+    this.service.registerUser(this.signUpForm.value)
+    .subscribe(dato => {
+      this.router.navigate(['/layout/login']);
+    }, error => {
+      console.log('Este es el error desde angular: ' + error);
+    }, () => {
+      console.log('Usuario creado Correctamente');
+    });
   }
 
 }
